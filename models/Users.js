1c9a21c4aeee11ffb39e1772a71df2b71b4f9121
DoAle34411 <domenica.arcos.lazo@udla.edu.ec>
2024-11-07 18:31:39 -0500

    feat: added isAdmin to users

--- models/Users.js
+++ models/Users.js
@@ -11,6 +11,7 @@ const userSchema = new Schema({
   phoneNumber: { type: String, required: true },
   password: { type: String, required: true },
   gender: { type: String, required: true },
+  isAdmin:{ type: Boolean, required: true, default: false}
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
