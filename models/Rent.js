4ea22071e07cd20a50215b84558384e256765876
DoAle34411 <domenica.arcos.lazo@udla.edu.ec>
2024-11-30 02:37:06 -0500

    fix added Users reference in rents

--- models/Rent.js
+++ models/Rent.js
@@ -8,7 +8,7 @@ const bookSubSchema = new Schema({
 
 const rentSchema = new Schema({
   rent_id: { type: String, default: () => new mongoose.Types.ObjectId() },
-  user_id: { type: String, required: true, ref: 'User' },
+  user_id: { type: String, required: true, ref: 'Users' },
   start_date: { type: Date, default: Date.now },
   max_end_date: { type: Date, default: () => new Date(Date.now() + 15 * 24 * 60 * 60 * 1000) },
   return_date: { type: Date, default: null },
